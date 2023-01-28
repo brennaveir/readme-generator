5ba9a3b40204c80451fcf2f89c55497a5991eefe
brennaveir <brennaveir@hotmail.com>
2023-01-27 23:13:21 -0500

    added user input questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is your application title?',
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'What is the description?',
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'What are the installation instructions?',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'What is the usage information?',
+        },
+        {
+          type: 'input',
+          name: 'contributors',
+          message: 'Who are the contributors?',
+        },
+        {
+          type: 'input',
+          name: 'test',
+          message: 'What are the test instructions?',
+        }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
